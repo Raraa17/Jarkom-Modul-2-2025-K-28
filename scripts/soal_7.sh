1d3bf09a34125922d6d2f81183c7004878bf13e6
Asarumawari <runnirattancraft@gmail.com>
2025-10-23 06:22:47 +0700

    Update soal_7.sh
    Updated DNS records and added dig commands for testing.

--- scripts/soal_7.sh
+++ scripts/soal_7.sh
@@ -1,9 +1,15 @@
-sirion     IN      A       192.225.3.2
-lindon     IN      A       192.225.3.5
-vingilot   IN      A       192.225.3.6
-
+#Tirion di /etc/bind/zones/db.K28.com
 www        IN      CNAME   sirion.K01.com.
 static     IN      CNAME   lindon.K01.com.
 app        IN      CNAME   vingilot.K01.com.
 
-rndc reload
+dig @192.225.3.3 www.K28.com
+
+#Valmar
+killall named
+/usr/sbin/named -u bind
+dig @192.225.3.4 www.K28.com
+dig @192.225.3.4 static.K28.com
+dig @192.225.3.4 app.K28.com
+
+#uji dig serupa pada node lain
