6616203acd386f4e6b104c77b0570bfee8384da9
Asarumawari <runnirattancraft@gmail.com>
2025-10-23 06:22:13 +0700

    Update soal_6.sh

--- scripts/soal_6.sh
+++ scripts/soal_6.sh
@@ -1,9 +1,9 @@
 #Tirion
-dig @localhost SOA K28.com  
-journalctl -u bind9 | grep notify  
+rndc notify K28.com
 
 #Valmar
-dig @localhost SOA K28.com  
-journalctl -u bind9 | grep transfer  
-ls /var/cache/bind/db.K28.com  
+killall named # kalau belum bisa, apt install psmisc -y
+/usr/sbin/named -u bind
+
+dig @192.225.3.4 K28.com SOA
 
