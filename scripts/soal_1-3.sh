933cacea419e98ee34ba3abbfde7bfe23c49db59
Asarumawari <runnirattancraft@gmail.com>
2025-10-23 06:20:42 +0700

    Update Soal_1-3.sh

--- scripts/soal_1-3.sh
+++ scripts/soal_1-3.sh
@@ -12,3 +12,12 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #Other nodes
 #nano /root/.bashrc
 echo "nameserver 192.168.122.1" > /etc/resolv.conf
+
+#Tes localhost
+ping 8.8.8.8
+
+#Tes internet
+ping google.com
+
+#Tes IP melalui node lain, misal IP Earendil melalui Erlond
+ping 192.225.1.2
